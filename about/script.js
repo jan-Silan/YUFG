c9c5a30f43af840666ab60b64e1b214bbc473d93
jan-Silan <176239473+jan-Silan@users.noreply.github.com>
2025-05-03 20:57:12 +0100

    See previous commit

---
+++ about/script.js
@@ -0,0 +1,17 @@
+let navLinks = document.getElementById('navbar_hamburger');
+let narrowLinks = document.getElementById('navbar_links_small');
+let burgerGap = document.getElementById('burger_gap');
+let gradientRectangle = document.getElementById('gradient_rectangle');
+
+var divHeight = document.getElementById('actual_page_contents').offsetHeight;
+var divLoc = document.getElementById('actual_page_contents').offsetTop;
+gradientRectangle.style.height = divLoc + divHeight + "px";
+navLinks.addEventListener('click', toggle);
+
+function toggle() {
+    narrowLinks.classList.toggle('hidden');
+    burgerGap.classList.toggle('hidden');
+    var divHeight = document.getElementById('actual_page_contents').offsetHeight;
+    var divLoc = document.getElementById('actual_page_contents').offsetTop;
+    gradientRectangle.style.height = divLoc + divHeight + "px";
+}
